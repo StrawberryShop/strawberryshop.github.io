bdadf5840cefbe59700184594209ec33bbaa530c
StrawberryShop <751848813@qq.com>
2024-07-25 23:11:48 +0800

    update:new blog

--- scripts/constant/docsIndex.js
+++ scripts/constant/docsIndex.js
@@ -1,6 +1,6 @@
 // AUTO-CREATE
-const ARCHIEVE_TREE={'name': 'docs', 'type': 'dir', 'children': [{'name': '测试文档', 'type': 'dir', 'children': [{'name': 'helloworld', 'href': 'docs/测试文档/helloworld.md', 'type': 'file', 'time': '2024-07-25 22:17:06'}]}]}
-const TOP_LIST=[{'name': 'helloworld', 'href': '/docs/测试文档/helloworld.md', 'type': 'file', 'time': '2024-07-25 22:17:06'}]
+const ARCHIEVE_TREE={'name': 'docs', 'type': 'dir', 'children': [{'name': '文档', 'type': 'dir', 'children': [{'name': '希希酱', 'href': 'docs/文档/希希酱.md', 'type': 'file', 'time': '2024-07-25 23:04:51'}]}, {'name': '测试文档', 'type': 'dir', 'children': [{'name': 'helloworld', 'href': 'docs/测试文档/helloworld.md', 'type': 'file', 'time': '2024-07-25 22:17:06'}]}]}
+const TOP_LIST=[{'name': '希希酱', 'href': '/docs/文档/希希酱.md', 'type': 'file', 'time': '2024-07-25 23:04:51'}, {'name': 'helloworld', 'href': '/docs/测试文档/helloworld.md', 'type': 'file', 'time': '2024-07-25 22:17:06'}]
 
 export {
     ARCHIEVE_TREE,
